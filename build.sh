b7932ee5cee63a859a4f422bbc3d9bbb1a8d337a
Just van den Broecke <just@justobjects.nl>
2019-11-11 17:08:40 +0100

    rename docker image to justb4/mapfish-print2

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 #
 # Build MapFish Print2 Docker image with options
 MFPRINT_VERSION="2.1-SNAPSHOT"
-docker build --build-arg IMAGE_TIMEZONE="Europe/Amsterdam" --build-arg MFPRINT_VERSION=${MFPRINT_VERSION} -t justb4/mapfish-print:${MFPRINT_VERSION} .
+docker build --build-arg IMAGE_TIMEZONE="Europe/Amsterdam" --build-arg MFPRINT_VERSION=${MFPRINT_VERSION} -t justb4/mapfish-print2:${MFPRINT_VERSION} .
